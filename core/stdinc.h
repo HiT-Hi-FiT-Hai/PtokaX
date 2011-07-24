b6d5975234fb9307a89c1fd910ccfa4d4acf3d31
PPK <PPK@4c629181-62e4-40b4-9e0d-b9f719119ca2>
2011-07-24 18:43:54 +0000

    Fixed: Conversion of string to 64 unsigned integer.
    git-svn-id: svn://svn.czdc.org/PtokaX@59 4c629181-62e4-40b4-9e0d-b9f719119ca2

--- core/stdinc.h
+++ core/stdinc.h
@@ -96,7 +96,7 @@
 #include "pxstring.h"
 //---------------------------------------------------------------------------
 #define PtokaXVersionString "0.4.1.2"
-#define BUILD_NUMBER "213"
+#define BUILD_NUMBER "214"
 #define sErrOutOfMem "[ERR] Out of memory..."
 
 #ifdef _WIN32
